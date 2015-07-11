ae9fff3794b5dc782695614d22b446204b8eda91
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-07-11 17:12:28 +0530

    feat(Flatline::with_stdio)

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,12 @@ pub struct Flatline<R: io::Read, W: io::Write> {
     reader: R,
     writer: W
 }
+
+impl Flatline<io::Stdin, io::Stdout> {
+    pub fn with_stdio() -> Flatline<io::Stdin, io::Stdout> {
+        Flatline {
+            reader: io::stdin(),
+            writer: io::stdout()
+        }
+    }
+}
